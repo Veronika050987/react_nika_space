d3f3d6f41b7ebf9dae06ebae65387bc52416037b
Veronika050987 <veronicaviv@mail.ru>
2026-08-04 18:13:35 +0300

    backgorund

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import PacketRaceGame from './PacketRaceGame';
+import back2 from './back2.jpg';
 
 function App() {
   return (
     <div style={{ 
-      boxSizing: 'border-box',
-        backgroundColor: '#0f0f1a', 
+        boxSizing: 'border-box',
+        // backgroundColor: '#0f0f1a', 
+        backgroundImage: `url(${back2})`,
+        backgroundSize: 'cover',
         minHeight: '100vh', 
         maxWidth: '100%',
         display: 'flex', 
         justifyContent: 'center', 
         alignItems: 'center', 
-        padding: '20px',
+        // padding: '20px',
         overflow: 'hidden' 
  }}> 
     <PacketRaceGame /> 
